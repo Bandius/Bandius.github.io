7c775abcd33b1a855581020be989b409e402291c
Ivan <47182740+Bandius@users.noreply.github.com>
2021-02-17 11:38:38 +0100

    stuff

--- index.js
+++ index.js
@@ -53,3 +53,28 @@ function newElement() {
     }
   }
 }
+function newElementProgress() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInputProg").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myULProg").appendChild(li);
+  }
+  document.getElementById("myInputProg").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
